69500fa05dbcf557122a933ac794dcd5bc0228ba
MrSofty123 <shaheer1642@gmail.com>
2023-03-04 07:09:32 +0500

    Update serviceWorker.js

--- public/serviceWorker.js
+++ public/serviceWorker.js
@@ -1,4 +1,4 @@
-let CACHE_NAME = 'my-site-cache-v1';
+let CACHE_NAME = 'site-cache-v1.1';
 const urlsToCache = [
     '/',
     '/index.html',
@@ -24,19 +24,19 @@ self.addEventListener('fetch', function (event) {
         })
     );
 });
-self.addEventListener('activate', function(event) {
-    event.waitUntil(
-        caches.keys().then(function(cacheNames) {
-            return Promise.all(
-                cacheNames.filter(function(cacheName) {
-                    // Return true if you want to remove this cache,
-                    // but remember that caches are shared across
-                    // the whole origin
-                    return true
-                }).map(function(cacheName) {
-                    return caches.delete(cacheName);
-                })
-            );
-        })
-    );
-  });
+// self.addEventListener('activate', function(event) {
+//     event.waitUntil(
+//         caches.keys().then(function(cacheNames) {
+//             return Promise.all(
+//                 cacheNames.filter(function(cacheName) {
+//                     // Return true if you want to remove this cache,
+//                     // but remember that caches are shared across
+//                     // the whole origin
+//                     return true
+//                 }).map(function(cacheName) {
+//                     return caches.delete(cacheName);
+//                 })
+//             );
+//         })
+//     );
+//   });
