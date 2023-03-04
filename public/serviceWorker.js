fa302dc48b90eb50775046439ec2e2e662f82bb1
MrSofty123 <shaheer1642@gmail.com>
2023-03-04 07:38:13 +0500

    fix

--- public/serviceWorker.js
+++ public/serviceWorker.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'site-cache-v1.2';
+const CACHE_NAME = 'site-cache-v1.3';
 const urlsToCache = [
     '/',
     '/index.html',
@@ -29,44 +29,47 @@ self.addEventListener('activate', event => {
     event.waitUntil(
         (async () => {
         const keys = await caches.keys();
-        return keys.map(async (cache) => {
+        var do_reload = false
+        keys.map(async (cache) => {
             if(cache !== CACHE_NAME) {
-            console.log('Service Worker: Removing old cache:',cache);
-            return await caches.delete(cache);
+                do_reload = true
+                console.log('Service Worker: Removing old cache:',cache);
+                return await caches.delete(cache);
             }
         })
+        if (do_reload) window.location.reload()
         })()
     )
 })
 
-// Scripts for firebase and firebase messaging
-importScripts('https://www.gstatic.com/firebasejs/9.0.0/firebase-app-compat.js');
-importScripts('https://www.gstatic.com/firebasejs/9.0.0/firebase-messaging-compat.js');
+// // Scripts for firebase and firebase messaging
+// importScripts('https://www.gstatic.com/firebasejs/9.0.0/firebase-app-compat.js');
+// importScripts('https://www.gstatic.com/firebasejs/9.0.0/firebase-messaging-compat.js');
 
-// Initialize the Firebase app in the service worker by passing the generated config
-const firebaseConfig = {
-    apiKey: "AIzaSyAxDsMVypgqkrY31cJici7P-KsISDkb5-Y",
-    authDomain: "gaussprime-2e46e.firebaseapp.com",
-    projectId: "gaussprime-2e46e",
-    storageBucket: "gaussprime-2e46e.appspot.com",
-    messagingSenderId: "527087624830",
-    appId: "1:527087624830:web:117b4bb3f5de6de5ad6e2f",
-    measurementId: "G-WFL1P9NTJX"
-};
+// // Initialize the Firebase app in the service worker by passing the generated config
+// const firebaseConfig = {
+//     apiKey: "AIzaSyAxDsMVypgqkrY31cJici7P-KsISDkb5-Y",
+//     authDomain: "gaussprime-2e46e.firebaseapp.com",
+//     projectId: "gaussprime-2e46e",
+//     storageBucket: "gaussprime-2e46e.appspot.com",
+//     messagingSenderId: "527087624830",
+//     appId: "1:527087624830:web:117b4bb3f5de6de5ad6e2f",
+//     measurementId: "G-WFL1P9NTJX"
+// };
 
-firebase.initializeApp(firebaseConfig);
+// firebase.initializeApp(firebaseConfig);
 
-// Retrieve firebase messaging
-const messaging = firebase.messaging();
+// // Retrieve firebase messaging
+// const messaging = firebase.messaging();
 
-messaging.onBackgroundMessage(function(payload) {
-  console.log('Received background message ', payload);
+// messaging.onBackgroundMessage(function(payload) {
+//   console.log('Received background message ', payload);
 
-  const notificationTitle = payload.notification.title;
-  const notificationOptions = {
-    body: payload.notification.body,
-  };
+//   const notificationTitle = payload.notification.title;
+//   const notificationOptions = {
+//     body: payload.notification.body,
+//   };
 
-  self.registration.showNotification(notificationTitle,
-    notificationOptions);
-});
+//   self.registration.showNotification(notificationTitle,
+//     notificationOptions);
+// });
