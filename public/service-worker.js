ca21114fe5f52076d98f4bde3c0b74150a3a475c
MrSofty123 <shaheer1642@gmail.com>
2023-03-04 20:35:05 +0500

    pwa update

---
+++ public/service-worker.js
@@ -0,0 +1,49 @@
+const CACHE_NAME = 'site-cache-v1.4';
+const toCache = [
+    '/',
+    '/index.html',
+];
+self.addEventListener('install', function(event) {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(function(cache) {
+        return cache.addAll(toCache)
+      })
+      .then(self.skipWaiting())
+  )
+})
+
+self.addEventListener('fetch', function(event) {
+  event.respondWith(async function() {
+    const cache = await caches.open(CACHE_NAME)
+    const cacheMatch = await cache.match(event.request)
+
+    if (navigator.onLine) {
+      const request = fetch(event.request)
+
+      event.waitUntil(async function() {
+        const response = await request
+        await cache.put(event.request, await response.clone())
+      }())
+
+      return cacheMatch || request
+    }
+
+    return cacheMatch
+  }())
+})
+
+self.addEventListener('activate', function(event) {
+  event.waitUntil(
+    caches.keys()
+      .then((keyList) => {
+        return Promise.all(keyList.map((key) => {
+          if (key !== CACHE_NAME) {
+            console.log('[ServiceWorker] Removing old cache:', key)
+            return caches.delete(key)
+          }
+        }))
+      })
+      .then(() => self.clients.claim())
+  )
+})
