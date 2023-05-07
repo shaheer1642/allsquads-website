3cf43bdd91cdf3d11c9e6415fd7ca0537e2becdd
MrSofty123 <shaheer1642@gmail.com>
2023-05-07 23:51:50 +0500

    Update cookie_handler.js

--- src/cookie_handler.js
+++ src/cookie_handler.js
@@ -1,10 +1,11 @@
-function putCookie(key, value, maxAge, path = '/', ) {
+function putCookie(key, value, maxAge = 2592000000, path = '/', ) {
     if (!getCookie('allow_cookies') || getCookie('allow_cookies') == 'false') return value
     document.cookie = `${key}=${value};path=${path};max-age=${maxAge}`;
     return value
 }
 
 function getCookie(name, default_value) {
+    console.log('[getCookie] called',document.cookie)
     const value = document.cookie.split('; ').find((row) => row.startsWith(`${name}=`))?.split('=')[1]
     if (value == undefined) return default_value
     if (value == 'true') return true
