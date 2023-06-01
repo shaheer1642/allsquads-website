b556cf6ca1cc4b2bbc43365fd60d21ad3bd3e4c0
MrSofty123 <shaheer1642@gmail.com>
2023-06-02 00:45:08 +0500

    Update server.js

--- server.js
+++ server.js
@@ -3,6 +3,31 @@ const app = new express()
 const path = require('path')
 const PORT = process.env.PORT || 3000
 
+app.get('/supportsofty',(req,res) => {
+  res.redirect('https://patreon.com/join/mrsofty')
+})
+
+app.get('/donatesofty',(req,res) => {
+  res.redirect('https://patreon.com/join/mrsofty')
+})
+
+app.use((req,res,next) => {
+  if (req.subdomains.length > 0) {
+    if (req.subdomains[0] == 'relic')
+      return res.redirect('https://discord.com/invite/brRTmrEeQv')  // erb server 
+    else if (req.subdomains[0] == 'relics')
+      return res.redirect('https://discord.com/invite/brRTmrEeQv')  // erb server 
+    else if (req.subdomains[0] == 'discord')
+      return res.redirect('https://discord.com/invite/brRTmrEeQv')  // erb server 
+    else if (req.subdomains[0] == 'www')
+      return res.redirect('https://discord.com/invite/Kyf6NAuEsa')  // allsquads server
+    else if (req.subdomains[0] == 'dev')
+      return next()
+  } else {
+    return res.redirect('https://discord.com/invite/Kyf6NAuEsa')  // allsquads server
+  }
+})
+
 app.use(express.static(path.join(__dirname, 'build')))
 
 app.get("*", (req, res) => {
